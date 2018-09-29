b336c85484595de345bf67f4c750ff368c20efc6
Alexey Noskov <vokson@uandex.ru>
2018-09-29 10:38:05 +0300

    Build 1.8.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -128,6 +128,15 @@
 
             <div class="row actual_version_color">
               <div class="col-1">
+                <p>1.8.0</p>
+              </div>
+              <div class="col-4">
+                <p>Добавлена "метла" в Log.</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-1">
                 <p>1.7.0</p>
               </div>
               <div class="col-4">
@@ -233,7 +242,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.7.0'
+      version: '1.8.0'
     };
   },
 
