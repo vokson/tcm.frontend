7fe8030cdb2cb4691807e690b96f01b273fa6d7c
Alexey Noskov <vokson@uandex.ru>
2018-12-20 05:13:10 +0300

    Home revised

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -172,6 +172,15 @@
 
       <div class="row actual_version_color">
         <div class="col-1">
+          <p>1.16.0</p>
+        </div>
+        <div class="col-4">
+          <p>В статистику добавлен график зарегистрированных титулов</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
           <p>1.15.2</p>
         </div>
         <div class="col-4">
@@ -394,7 +403,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.15.2'
+      version: '1.16.0'
     };
   },
 
