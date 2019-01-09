93e90a0eda05408bfeedba196f46b6911d146247
Alexey Noskov <vokson@uandex.ru>
2019-01-09 16:12:49 +0300

    Words added to Log

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -172,6 +172,15 @@
 
       <div class="row actual_version_color">
         <div class="col-1">
+          <p>1.18.1</p>
+        </div>
+        <div class="col-4">
+          <p>Добавлены слова на вкладке "Log"</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
           <p>1.18.0</p>
         </div>
         <div class="col-4">
@@ -421,7 +430,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.18.0'
+      version: '1.18.1'
     };
   },
 
