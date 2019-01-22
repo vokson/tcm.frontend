5e0f19a19cff72a1b423c39043f458df2698ac84
Alexey Noskov <vokson@uandex.ru>
2019-01-22 16:47:24 +0300

    Build 1.20.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -172,6 +172,15 @@
 
       <div class="row actual_version_color">
         <div class="col-1">
+          <p>1.20.1</p>
+        </div>
+        <div class="col-4">
+          <p>В проверяшку добавлено расширение</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
           <p>1.20.0</p>
         </div>
         <div class="col-4">
@@ -448,7 +457,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.20.0'
+      version: '1.20.1'
     };
   },
 
