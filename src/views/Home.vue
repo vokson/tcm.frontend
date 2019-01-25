7e9b2e6433a621a52c9eed8108f57d6b9ad5617d
Alexey Noskov <vokson@uandex.ru>
2019-01-25 10:33:51 +0300

    Build 1.22.1_2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -172,6 +172,15 @@
 
       <div class="row actual_version_color">
         <div class="col-1">
+          <p>1.22.1</p>
+        </div>
+        <div class="col-4">
+          <p>Исправлена ошибка вывода записей в Log при нажатии на титул в Title</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
           <p>1.22.0</p>
         </div>
         <div class="col-4">
@@ -484,7 +493,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.22.0'
+      version: '1.22.1'
     };
   },
 
