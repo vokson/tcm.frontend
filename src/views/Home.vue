beff72d0e0b0dce4b9288e789d877a5955451809
Alexey Noskov <vokson@uandex.ru>
2019-04-23 08:16:06 +0300

    Count of files has been added to Sender

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -176,6 +176,15 @@
 
       <div class="row actual_version_color">
         <div class="col-1">
+          <p>1.30.1</p>
+        </div>
+        <div class="col-4">
+          <p>В Отправку добавлен показ общего числа файлов в папке</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
           <p>1.30.0</p>
         </div>
         <div class="col-4">
@@ -605,7 +614,7 @@ export default {
       choose_language: "",
       new_password_1: "",
       new_password_2: "",
-      version: '1.30.0'
+      version: '1.30.1'
     };
   },
 
